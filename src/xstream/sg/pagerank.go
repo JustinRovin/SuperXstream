468b37d509608a69a0193b7c94b22056263f396a
Brandon Bevans <brandonbevans@gmail.com>
2015-05-29 15:39:38 -0700

    Starting to port pagerank i got dis

---
+++ src/xstream/sg/pagerank.go
@@ -0,0 +1,30 @@
+package sg
+
+import (
+	"io"
+	"log"
+	"math"
+	"os"
+	"unsafe"
+
+	"github.com/ncw/directio"
+)
+
+type prVertexT struct {
+	degree uint32
+	rank   float32
+	sum    float32
+}
+
+type prUpdateT struct {
+	target uint32
+	rank   float32
+}
+
+type PREngine struct {
+	Base BaseEngine
+}
+
+func (self *PREngine) Scatter(phase uint32) error {
+	//Unpack parameters
+}
