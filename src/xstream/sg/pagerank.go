6d909b95a32072c07ece88d86639ea4e582d99ce
Justin Rovin <justinrovin@gmail.com>
2015-05-30 05:28:44 -0700

    pulling for updates

--- src/xstream/sg/pagerank.go
+++ src/xstream/sg/pagerank.go
@@ -10,18 +10,17 @@ import (
 	"github.com/ncw/directio"
 )
 
-
 const dampingFactor int = 0.85
 
 type prVertexT struct {
-	degree uint32
-	rank   float32
-	sum    float32
+	degree       uint32
+	rank         float32
+	contribution float32
 }
 
 type prUpdateT struct {
-	target uint32
-	rank   float32
+	target       uint32
+	contribution float32
 }
 
 type PREngine struct {
@@ -34,7 +33,7 @@ type PREngine struct {
 func (self *PREngine) AllocateVertices() error {
 	log.Println("vertexoffset", self.Base.vertexOffset)
  	self.Base.vertexOffset = uint32(self.Base.Partition * self.Base.NumVertices)
- 	self.vertices = make([]bfsVertexT, self.Base.NumVertices)
+ 	self.vertices = make([]prVertexT, self.Base.NumVertices)
  
  	return nil
  }
@@ -43,7 +42,32 @@ func (self *PREngine) AllocateVertices() error {
 //vert pagerank to 100/(number of verts), so each vert
 //get equal pagerankl
 
-func init()
+func (self *PREngine) Init(phase uint32) error {
+	if phase == 0 {
+		var startRank float32 = float32(100/self.Base.NumVerticies)
+
+		//we need a mapping for vertices here
+		for _, v := range self.Base.NumVertices{
+			self.vertices[i] = prVertexT{
+				degree: 0, 
+				rank: startRank,
+				contribution: 0,
+			}
+		}
+
+
+
+
+		if self.Base.Partition == 0 {
+			self.vertices[0].parent = 0
+		}
+
+		self.proceed = true
+	}
+
+	return nil
+}
+
 
 //for a node, see which out edges point to me
 //
